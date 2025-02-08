6e641a32e78e353a043d698c8a0258eaf2e116a1
AndresVargas3 <139194310+AndresVargas3@users.noreply.github.com>
2025-02-08 13:52:18 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Segura???",
+    "Piensalo bienn?",
+    "Ya lo pensaste???",
+    "Porfaaa",
+    "Porfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    "Si dices que no voy a llorar",
+    "Voy a llorar",
+    "Me voy a matar",
+    "Ok",
+    "Di que siiiiiiiiiiiiiiii"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
